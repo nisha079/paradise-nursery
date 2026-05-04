cbcecbf7cb4939a016fd66b11d8c0f0c45fa062d
nisha079 <bagalnisha079@gmail.com>
2026-05-04 17:36:30 +0530

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your one-stop destination for indoor and outdoor plants.
+      </p>
+      <p>
+        We help make your home greener and healthier.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
