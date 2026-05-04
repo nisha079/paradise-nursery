bba83a0ac6bb5a4e75cbed206bc81bd12cd41f10
nisha079 <bagalnisha079@gmail.com>
2026-05-04 17:38:53 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Paradise Nursery 🌿</h1>
+      <p>Welcome to the best plant shop!</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
